023bd1a286195e67d1634e42401d1c64b6aa3a82
Jummy-Datasci <olajumoke2603@gmail.com>
2025-05-14 12:34:27 +0100

    First upload

---
+++ intro_to_python.py
@@ -0,0 +1,30 @@
+Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Welcome to Data Science with Python
+>>> print("Hello, Data Science World!")
+Hello, Data Science World!
+>>> 
+>>> # Variables
+>>> name = "Jumoke"
+>>> goal = "Become a top Data Scientist"
+>>> print(f"My name is {name}, and my goal is to {goal}.")
+My name is Jumoke, and my goal is to Become a top Data Scientist.
+>>> 
+>>> # Basic Math
+>>> a = 10
+>>> b = 5
+>>> print("sum:", a+b)
+sum: 15
+>>> print("product:", a*b)
+product: 50
+>>> 
+>>> # list of your goals
+>>> goals = ["Finish M.sc. with distinction","Get scholarships", "Build portfolio","Get hired abroad"]
+>>> for g in goals:
+...     print("Goal:",g)
+... 
+...     
+Goal: Finish M.sc. with distinction
+Goal: Get scholarships
+Goal: Build portfolio
+Goal: Get hired abroad
